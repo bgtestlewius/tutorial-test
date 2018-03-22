718b677c3f0e2d9cc8cd6a61c52f28091b3634f4
bgtestlewius <williampgillespie@lewisu.edu>
2018-03-22 08:12:58 -0500

    Implement forst green oval

--- src/OvalDraw.java
+++ src/OvalDraw.java
@@ -1,3 +1,8 @@
+// Author:	Bill Gillespie
+// Note:	Drawing oval in forest green(35,142,35)
+
+
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
@@ -9,14 +14,14 @@ import java.awt.Color;
 class Oval extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		System.out.println("Paint Compoment");
+		//System.out.println("Paint Compoment");
 		int panelWidth = getWidth();
 		int panelHeight = getHeight();
 		
-		g.setColor(new Color(0,0,255));
+		g.setColor(new Color(35,142,35));
 		g.fillOval(0,0,panelWidth,panelHeight);
 		
-		System.out.println("Paint Compoment");
+		//System.out.println("Paint Compoment");
 		
 		
 	}
@@ -25,7 +30,6 @@ class Oval extends JPanel {
 public class OvalDraw {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 
 		JFrame myFrame = new JFrame();
@@ -36,7 +40,7 @@ public class OvalDraw {
 		
 		Oval myOval = new Oval();
 		Container contentPane = myFrame.getContentPane();
-		System.out.print("test");
+		//System.out.print("test");
 		contentPane.add(myOval);
 		
 		
