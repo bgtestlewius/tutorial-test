256562707fec232c48aa631ceb9755f2eb935339
bgtestlewius <williampgillespie@lewisu.edu>
2018-03-21 23:50:01 -0500

    implement v0-01

--- src/OvalDraw.java
+++ src/OvalDraw.java
@@ -1,9 +1,18 @@
+import javax.swing.JFrame;
+
 
 public class OvalDraw {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("test");
 
+		JFrame myFrame = new JFrame();
+		myFrame.setBounds(200,200,400,800);
+		myFrame.setTitle("Draw");
+		myFrame.setVisible(true);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 	}
 
 }
